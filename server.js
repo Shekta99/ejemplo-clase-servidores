a2aa3f1981e2c0a6d2b1490c2c27c84fd3f63803
Sergio Marentes <sergiomarentes705@gmail.com>
2023-09-19 21:25:26 -0500

    configuring /user route

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const host = "localhost";
 const PORT = 8080;
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.write(JSON.stringify({ message: "hola mundo" }));
-  res.end();
+  // url https://docs.google.com/presentation/d/19yh2H8ik0GBL9GNr7A2sqiEidO8OcrAQ0P6dKm0JpAs/edit#slide=id.g196cbed37f9_0_0
+  const url = new URL(req.url, `http://${host}:${PORT}`);
+  if (url.pathname === "/user") {
+    res.writeHead(200);
+    res.write(JSON.stringify({ message: "hola mundo desde la ruta user" }));
+    res.end();
+  } else {
+    res.writeHead(200);
+    res.write(JSON.stringify({ message: "hola mundo" }));
+    res.end();
+  }
 };
 
 const server = http.createServer(requestListener);
